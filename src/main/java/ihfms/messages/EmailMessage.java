3c8f0cb3c1b54df4076ae427c0174c9d5562445e
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-03-24 16:27:11 +0300

    working product

--- src/main/java/ihfms/messages/EmailMessage.java
+++ src/main/java/ihfms/messages/EmailMessage.java
@@ -1,8 +1,19 @@
 package ihfms.messages;
 
 public class EmailMessage implements Message {
+    private String content;
     @Override
     public void send() {
         // Email sending logic
+    }
+
+    @Override
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String getContent() {
+        return this.content;
     }
 }
--- src/main/java/ihfms/messages/SMSMessage.java
+++ src/main/java/ihfms/messages/SMSMessage.java
@@ -1,8 +1,19 @@
 package ihfms.messages;
 
 public class SMSMessage implements Message {
+    private String content;
     @Override
     public void send() {
         // SMS sending logic
+    }
+
+    @Override
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String getContent() {
+        return this.content;
     }
 }
