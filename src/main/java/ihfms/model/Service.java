fb331aa2c5e5b2a68781fe17dd7c5f8e422cda68
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-04-10 11:33:49 +0300

    service model

---
+++ src/main/java/ihfms/model/Service.java
@@ -0,0 +1,38 @@
+package ihfms.model;
+
+public class Service {
+    private String serviceId;
+    private String description;
+    private double cost;
+
+    public Service(String serviceId, String description, double cost) {
+        this.serviceId = serviceId;
+        this.description = description;
+        this.cost = cost;
+    }
+
+    // Getters and setters
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(String serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
